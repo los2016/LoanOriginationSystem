93a0a3478e4df8629b138b8550f6d4cca19b0385
los2016 <anirban.biswas@cognizant.com>
2016-04-13 13:29:40 +0530

    load questions

---
+++ LoanOriginationSystem/LOSServices/src/main/java/com/myorg/losservices/util/CreateQuestionDOM.java
@@ -0,0 +1,38 @@
+package co.myorg.util;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.net.URISyntaxException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.w3c.dom.Document;
+import org.xml.sax.SAXException;
+
+public class CreateQuestionDOM {
+
+	final String questionnairePath = "";
+	
+	public static Document getQuestionsDOM(final String category) throws SAXException,
+			ParserConfigurationException, IOException, URISyntaxException {
+		Document dom = null;
+		File questionFile = null;
+
+		questionFile = new File(questionnairePath + category + "-questionnaire.xml");
+		System.out.println("Question File Absolute Path " + questionFile.getAbsolutePath());
+
+		DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+		DocumentBuilder db = dbf.newDocumentBuilder();
+		try {
+			dom = db.parse(questionFile);
+		} catch (FileNotFoundException fileNotFound) {
+			System.out.println("Error : Question File Not Found " + fileNotFound);
+		}
+		dom.getDocumentElement().normalize();
+		return dom;
+	}
+
+}
---
+++ LoanOriginationSystem/LOSServices/src/main/java/com/myorg/losservices/util/LoadQuestion.java
@@ -0,0 +1,84 @@
+package co.myorg.util;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
+import org.w3c.dom.Node;
+import org.xml.sax.SAXException;
+
+import com.myorg.losservices.model.Answer;
+import com.myorg.losservices.model.Question;
+
+public class LoadQuestion {
+	
+	Document dom;
+	private ArrayList<Question> questionList = new ArrayList<Question>();
+	
+	public LoadQuestion(final String category) 
+		throws SAXException,ParserConfigurationException,IOException, URISyntaxException{
+		dom = CreateQuestionDOM.getDOM(category);
+	}
+	
+	public populateQuestionList(final String category) { 
+		String options[] = new String[4];
+		String question = null;
+	    String answerType = null;
+	    String required = null;
+	    //TO DO no of question loop
+		for (int i=0; i<=3; i++) {
+			NodeList qList = dom.getElementsByTagName("question");
+			NodeList childList = qList.item(i).getChildNodes();
+	    
+			int counter=0;
+	    
+			for (int j = 0; j < childList.getLength(); j++) {
+				Node childNode = childList.item(j);
+				if ("answer".equals(childNode.getNodeName()))
+				{
+					options[counter] = childList.item(j).getTextContent();
+					counter++;
+				}
+				else if("questionnaire".equals(childNode.getNodeName()))
+				{
+					question = childList.item(j).getTextContent();
+				}
+				else if("answerType".equals(childNode.getNodeName()))
+				{
+					answerType = childList.item(j).getTextContent();
+				}
+				else if("required".equals(childNode.getNodeName()))
+				{
+					required = childList.item(j).getTextContent();
+				}
+            
+			}
+			
+			Question ques = new Question();
+			ques.setQnsId(category+i); //category+questionNb will be questionID
+			ques.setQnsDecs(question);
+			
+			List<Answer> answerList = new ArrayList<Answer>();
+			for(String ans:options)
+			{
+				Answer ans = new Answer();
+				ans.setAnsId(); //TODO set answer id and populate other fields answerType and required flag
+				ans.setAnsDesc(ans);    
+				ans.setAnsValue(ans);
+				answerList.add(ans);
+			}
+			ques.setAnswerList(answerList);
+			questionList.add(ques);
+		}	
+	}
+	
+	public ArrayList<Question> getQuestionList(){
+		return this.questionList;
+	}
+}

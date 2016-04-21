5bc0da1afd22122e9311f92a0cf3aa8e754acffd
Sathish Arumugam <sathishkumar.arumugam@cognizant.com>
2016-04-21 16:10:13 -0700

    LOS UI: Added 1) HTML files for login, home and list pages with header and footer separation 2) SCSS and CSS files 3) Images

---
+++ LoanOriginationSystem/LOSUI/src/main/webapp/app/components/home/ng-component.js
@@ -0,0 +1,10 @@
+"use strict";
+
+function homeController () {
+
+}
+
+angular.module("mainModule").component("appHome",{
+	templateUrl:"/app/components/home/ng-template.html",
+	controller: homeController
+});

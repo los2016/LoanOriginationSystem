4ad6d5a829f50e9fadee5361d483752500fae811
GaganGoswami <gagan.goswami@gmail.com>
2016-04-25 18:34:11 -0400

    Fixed UserTime stamp issue
    Fixed User Time stamp issues and Added logging in USERDAO and Coverage
    DAO

--- MortgageOriginationRestService/src/main/java/com/jpmorgan/awm/pb/mortgageorigination/dao/impl/CoverageDAOImpl.java
+++ MortgageOriginationRestService/src/main/java/com/jpmorgan/awm/pb/mortgageorigination/dao/impl/CoverageDAOImpl.java
@@ -4,7 +4,10 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Service;
@@ -18,24 +21,30 @@ public class CoverageDAOImpl implements CoverageDAO {
 	@Autowired
 	private JdbcTemplate jdbcTemplate;
 
+	Logger logger = LoggerFactory.getLogger(CoverageDAOImpl.class);
+
 	public CoverageResponse getClientOrAdvisorCoverage(String userType, String partyId) {
 		CoverageResponse coverageResponse = new CoverageResponse();
 
-		// coverageList.add(new Long(2341434l));
-		// coverageList.add(new Long(5678568l));
-		// coverageList.add(new Long(78782334l));
-		// coverageList.add(new Long(99234134l));
-		// coverageResponse.setCoverageList(coverageList);
+		logger.info("getClientOrAdvisorCoverage :: Entry::  userType: {} partyId : {}", userType, partyId);
 
 		String sql = "";
-		if (userType.equalsIgnoreCase("C")) {
-			sql = "select p.advisor_party_id as party_id from demo_client_team p where p.party_id = ?";
-		} else {
-			sql = "select p.party_id as party_id from demo_client_team p where p.ADVISOR_PARTY_ID = ?";
-		}
 
-		List<Long> coverageList = jdbcTemplate.query(sql,new Object[] { partyId },  new UsersRowMapper());
-		coverageResponse.setCoverageList(coverageList);
+		try {
+			if (userType.equalsIgnoreCase("C")) {
+				sql = "select p.advisor_party_id as party_id from demo_client_team p where p.party_id = ?";
+			} else {
+				sql = "select p.party_id as party_id from demo_client_team p where p.ADVISOR_PARTY_ID = ?";
+			}
+
+			List<Long> coverageList = jdbcTemplate.query(sql, new Object[] { partyId }, new UsersRowMapper());
+			coverageResponse.setCoverageList(coverageList);
+
+			logger.info("getClientOrAdvisorCoverage :: Exit ::  No of Parties : {}", coverageList.size());
+
+		} catch (DataAccessException e) {
+			e.printStackTrace();
+		}
 
 		return coverageResponse;
 	}

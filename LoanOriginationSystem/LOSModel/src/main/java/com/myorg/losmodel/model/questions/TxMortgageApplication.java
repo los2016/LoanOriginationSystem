ed0a5a94dd31d284a22ee6f0cdea2d87d6c1cd16
los2016 <anirban.biswas@cognizant.com>
2016-04-27 07:12:09 +0530

    Debasish : Restored back

---
+++ LoanOriginationSystem/LOSModel/src/main/java/com/myorg/losmodel/model/questions/TxMortgageApplication.java
@@ -0,0 +1,41 @@
+package com.myorg.losmodel.model.questions;
+
+public class TxMortgageApplication {
+
+	private int transactionId;
+	private String statusCode;
+	private String jsonString;
+	private int partyId;
+
+	public int getTransactionId() {
+		return transactionId;
+	}
+
+	public void setTransactionId(int transactionId) {
+		this.transactionId = transactionId;
+	}
+
+	public String getStatusCode() {
+		return statusCode;
+	}
+
+	public void setStatusCode(String statusCode) {
+		this.statusCode = statusCode;
+	}
+
+	public String getJsonString() {
+		return jsonString;
+	}
+
+	public void setJsonString(String jsonString) {
+		this.jsonString = jsonString;
+	}
+
+	public int getPartyId() {
+		return partyId;
+	}
+
+	public void setPartyId(int partyId) {
+		this.partyId = partyId;
+	}
+}
